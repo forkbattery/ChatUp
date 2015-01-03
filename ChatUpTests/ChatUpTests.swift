b09748bcde692e6e75b003cf72c1175d2f71160e
forkbattery <forkbattery@gmail.com>
2015-01-03 18:52:01 -0500

    first commit

---
+++ ChatUpTests/ChatUpTests.swift
@@ -0,0 +1,36 @@
+//
+//  ChatUpTests.swift
+//  ChatUpTests
+//
+//  Created by Kanishka Goel on 2015-01-03.
+//  Copyright (c) 2015 Kanishka Goel. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ChatUpTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
