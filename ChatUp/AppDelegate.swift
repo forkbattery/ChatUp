01006392a5dec3ef864c2cfbcd794aeba35e239c
forkbattery <forkbattery@gmail.com>
2015-01-03 21:28:46 -0500

    setup Parse and connected application

--- ChatUp/AppDelegate.swift
+++ ChatUp/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.setApplicationId("8Rwq01BJDdsVUn6Zr1jvAlMGFSBjdRFYxUcwoEHo", clientKey: "8Rwq01BJDdsVUn6Zr1jvAlMGFSBjdRFYxUcwoEHo")
+        
         return true
     }
 
---
+++ ChatUp/ChatUpHeader.m
@@ -0,0 +1,9 @@
+//
+//  ChatUpHeader.m
+//  ChatUp
+//
+//  Created by Kanishka Goel on 2015-01-03.
+//  Copyright (c) 2015 Kanishka Goel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
