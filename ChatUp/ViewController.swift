a3606464b0c8eb37e8690cd88e85d045fbfa4ab4
forkbattery <forkbattery@gmail.com>
2015-01-03 22:23:15 -0500

    Login view constraints set

--- ChatUp/ViewController.swift
+++ ChatUp/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //For UI Constraints
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var noAccountLabel: UILabel!
+    @IBOutlet weak var signupLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let theWidth    = view.frame.size.width;
+        let theHeight   = view.frame.size.height;
+        
+        titleLabel.center       = CGPointMake(theWidth/2, 130)
+        usernameText.frame      = CGRectMake(16, 200, theWidth - 32, 30)
+        passwordText.frame      = CGRectMake(16, 240, theWidth - 32, 30)
+        loginButton.center      = CGPointMake(theWidth/2, 330)
+        noAccountLabel.center   = CGPointMake(theWidth/2, theHeight - 60)
+        signupLabel.center      = CGPointMake(theWidth/2, theHeight - 30)
+        
     }
 
     override func didReceiveMemoryWarning() {
