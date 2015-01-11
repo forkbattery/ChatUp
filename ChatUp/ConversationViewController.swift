33048203ff27783d0cd21f0c2eda352ba4fe8688
forkbattery <forkbattery@gmail.com>
2015-01-11 00:33:17 -0500

    Implemented UI for conversation view

--- ChatUp/ConversationViewController.swift
+++ ChatUp/ConversationViewController.swift
@@ -13,10 +13,40 @@ var otherProfileName = ""
 
 class ConversationViewController: UIViewController {
 
+    @IBOutlet weak var resultsScrollView: UIScrollView!
+    @IBOutlet weak var frameMessageView: UIView!
+    @IBOutlet weak var lineLabel: UILabel!
+    @IBOutlet weak var messageTextView: UITextView!
+    @IBOutlet weak var sendButton: UIButton!
+    
+    var scrollViewOriginalY:CGFloat = 0
+    var frameMessageOriginalY: CGFloat = 0
+    
+    let mLabel = UILabel(frame: CGRectMake(5, 8, 200, 20))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let theWidth = view.frame.size.width
+        let theHeight = view.frame.size.height
+        
+        resultsScrollView.frame = CGRectMake(0, 64, theWidth, theHeight - 114)
+        resultsScrollView.layer.zPosition = 20
+        frameMessageView.frame = CGRectMake(0, resultsScrollView.frame.maxY, theWidth, 50)
+        lineLabel.frame = CGRectMake(0, 0, theWidth, 1)
+        messageTextView.frame = CGRectMake(2, 1, self.frameMessageView.frame.size.width - 50, 48)
+        sendButton.center = CGPointMake(frameMessageView.frame.size.width - 30, 24)
+        
+        scrollViewOriginalY = self.resultsScrollView.frame.origin.y
+        frameMessageOriginalY = self.frameMessageView.frame.origin.y
+        
+        self.title = otherProfileName
+        
+        mLabel.text = "Type a message..."
+        mLabel.backgroundColor = UIColor.clearColor()
+        mLabel.textColor = UIColor.lightGrayColor()
+        messageTextView.addSubview(mLabel)
+        
     }
 
     override func didReceiveMemoryWarning() {
